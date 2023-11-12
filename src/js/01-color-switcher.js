3059642b7b4f1e17b5612e331c7a38c8d79933af
orellesha9 <orellesha9@gmail.com>
2023-11-12 13:52:00 +0200

    finish_2.homwork

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    btnStart: document.querySelector('.btn_start'),
+    btnStop: document.querySelector('.btn_stop'),
+    bgrColor: document.querySelector('body')
+
+};
+const bodyColor = window.getComputedStyle(refs.bgrColor).backgroundColor;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+refs.btnStart.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        refs.btnStart.disabled = true;
+        const randomColor = getRandomHexColor();
+        refs.bgrColor.style.backgroundColor = randomColor;
+    }, 1000);
+});
+
+
+
+refs.btnStop.addEventListener('click', () => {
+    refs.btnStart.disabled = false;
+  clearInterval(timerId);
+});
 
---
+++ src/js/02-timer.js
@@ -0,0 +1,81 @@
+import flatpickr from 'flatpickr';
+
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+const refs = {
+  datePicker: document.getElementById('datetime-picker'),
+  btnStart: document.getElementById('start'),
+  dataDays: document.querySelectorAll('[data-days]'),
+  dataHours: document.querySelectorAll('[data-hours]'),
+  dataMinutes: document.querySelectorAll('[data-minutes]'),
+  dataSeconds: document.querySelectorAll('[data-seconds]'),
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+ 
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+let counters;
+const currentDateInMilliseconds = Date.now();
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= currentDateInMilliseconds) {
+      // window.alert('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future');
+      refs.btnStart.disabled = true;
+    } else {
+      refs.btnStart.disabled = false;
+      refs.btnStart.addEventListener('click', () => {
+        counters = setInterval(() => {
+          const currentDifference = selectedDates[0] - Date.now();
+
+          convertMs(currentDifference);
+          //! Пов'язуємо значення з змінними лічильника
+          const { days, hours, minutes, seconds } =
+            convertMs(currentDifference);
+
+          refs.dataDays.forEach(
+            element => (element.textContent = addLeadingZero(days))
+          );
+          refs.dataHours.forEach(
+            element => (element.textContent = addLeadingZero(hours))
+          );
+          refs.dataMinutes.forEach(
+            element => (element.textContent = addLeadingZero(minutes))
+          );
+          refs.dataSeconds.forEach(
+            element => (element.textContent = addLeadingZero(seconds))
+          );
+        }, 1000);
+      });
+    }
+  },
+};
+
+flatpickr(refs.datePicker, options);
