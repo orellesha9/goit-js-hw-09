2dddbb5ec2a2ef0b3145f3d5512d206826203611
orellesha9 <orellesha9@gmail.com>
2023-11-14 23:08:15 +0200

    finish_3.1_finish.homwork

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -15,6 +15,7 @@ const refs = {
 };
 
 function convertMs(ms) {
+  refs.btnStart.disabled = true;
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
@@ -36,46 +37,56 @@ function convertMs(ms) {
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
+
+
 let counters;
 const currentDateInMilliseconds = Date.now();
+
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  
   onClose(selectedDates) {
     if (selectedDates[0] <= currentDateInMilliseconds) {
       // window.alert('Please choose a date in the future');
       Notify.failure('Please choose a date in the future');
-      refs.btnStart.disabled = true;
+     
     } else {
       refs.btnStart.disabled = false;
+    
       refs.btnStart.addEventListener('click', () => {
+        refs.datePicker.disabled = true;
+      
         counters = setInterval(() => {
           const currentDifference = selectedDates[0] - Date.now();
 
-          convertMs(currentDifference);
-          //! Пов'язуємо значення з змінними лічильника
-          const { days, hours, minutes, seconds } =
-            convertMs(currentDifference);
+          // Прибирати інтервал коли таймер доходить до 0
+          if (currentDifference <= 0) {
+            clearInterval(counters);
+            refs.datePicker.disabled = false;
+            console.log('Timer reached 0'); // Вмикаємо кнопку після досягнення 0
+          } else {
+            const { days, hours, minutes, seconds } = convertMs(currentDifference);
 
-          refs.dataDays.forEach(
-            element => (element.textContent = addLeadingZero(days))
-          );
-          refs.dataHours.forEach(
-            element => (element.textContent = addLeadingZero(hours))
-          );
-          refs.dataMinutes.forEach(
-            element => (element.textContent = addLeadingZero(minutes))
-          );
-          refs.dataSeconds.forEach(
-            element => (element.textContent = addLeadingZero(seconds))
-          );
+            refs.dataDays.forEach(
+              element => (element.textContent = addLeadingZero(days))
+            );
+            refs.dataHours.forEach(
+              element => (element.textContent = addLeadingZero(hours))
+            );
+            refs.dataMinutes.forEach(
+              element => (element.textContent = addLeadingZero(minutes))
+            );
+            refs.dataSeconds.forEach(
+              element => (element.textContent = addLeadingZero(seconds))
+            );
+          }
         }, 1000);
       });
     }
   },
-};
-
+}
 flatpickr(refs.datePicker, options);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,35 +7,38 @@ const refs = {
   form: document.querySelector('.form'),
 };
 
-
 refs.form.addEventListener('submit', event => {
   event.preventDefault();
-  const delay = parseInt(refs.delay.value, 10);
-  const step = parseInt(refs.step.value, 10); 
-  const amount = parseInt(refs.amount.value, 10);
- for (let i = 1; i <= amount; i++) {
-   const totalDelay = delay + i * step;
-   createPromise(i , totalDelay)
-   .then(({ position, delay }) => {
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-   
- 
 
-  }
-});
+  const initDelay = parseInt(refs.delay.value, 10);
+  const step = parseInt(refs.step.value, 10);
+  const amount = parseInt(refs.amount.value, 10);
+  let delay = initDelay;
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay);
+    delay += step;
+  };
+}
+)
+      
+  
 function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
-    }, delay);
-  });
+    const promise = new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  
+    promise.then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
 }
